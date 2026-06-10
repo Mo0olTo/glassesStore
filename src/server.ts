77f962195f50bec655f2d3317bd316c01b13b276
mo0olto <mostafa.allam.de@gmail.com>
2026-06-10 09:57:59 +0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
-import express from 'express';
-import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
+import app from './app';
+
 const PORT = process.env.PORT || 5000;
-app.use(cors());
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Glasses API Running');
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
-
-export default app;
